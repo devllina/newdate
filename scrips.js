00f7d5104f7d826522f19a95714d295300aa9188
Adelina <adelinatokonova232@gmail.com>
2025-12-11 20:02:18 +0600

    Add files via upload

---
+++ scrips.js
@@ -0,0 +1,27 @@
+//new date-Бул javascrit'теги  азыркы дата мн сааттын обьектиси Ал компьтердеги азыркы убакытты алып обьект ачат Функция же метод автомотически озу чыгарып берет
+//кантип new date ачабыз
+
+// const now = new Date()
+// console.log(now)
+//new date'теги методдор
+// let now= new Date()
+// console.log(now)
+// now.getFullYear()
+// console.log(now.getFullYear())//=> 2025
+//console.log(now.getMonth())//=> 11
+// Эмне учун 11 деп чыгарат?
+// анткени javascript 0 дон башталат 
+//[0,1,2,3,4,5,6,7,8,9,10,11,]0-январь
+// let now = new Date()
+// alert(now)
+// get day жуманын куну
+// get date - число 
+let now = new Date()
+// now.getFullYear
+console.log("Азыркы жыл:", now.getFullYear())
+console.log("Азыркы ай:", now.getMonth())
+console.log( "Апта күнү:", now.getDay())
+console.log("Азыркы күн:", now.getDate())
+console.log("Саат:", now.getHours())
+console.log("Минута:", now.getMinutes())
+console.log("Секунда:", now.getSeconds())
